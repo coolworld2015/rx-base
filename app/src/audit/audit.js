c8035c3fde9aae143ae3a2a305793c01ab600b99
coolworld2015 <wintermute2015@ukr.net>
2017-04-14 21:59:22 +0300

    [audit] - added handleScroll

--- app/src/audit/audit.js
+++ app/src/audit/audit.js
@@ -10,7 +10,9 @@ class Audit extends Component {
         this.state = {
             showProgress: false,
             items: appConfig.audit.items.slice(0, 20),
-			resultsCount: 0
+			resultsCount: 0,
+            recordsCount: 25,
+            positionY: 0
         };
     }
 	
@@ -24,7 +26,25 @@ class Audit extends Component {
 			this.getItems();
 		}
 	}
-
+	
+	handleScroll() {
+		var position = document.querySelector('.showMessages').scrollTop;
+        var items, positionY, recordsCount;
+        recordsCount = this.state.recordsCount;
+        positionY = this.state.positionY;
+		items = appConfig.audit.items.slice(0, recordsCount);
+		
+		if (position > positionY) {
+			console.log(items.length);
+			console.log(position);
+            this.setState({
+                items: items,
+                recordsCount: recordsCount + 20,
+                positionY: positionY + 1000
+            });
+        }
+	}
+	
     getItems() {
 		this.setState({
             showProgress: true
@@ -122,7 +142,7 @@ class Audit extends Component {
 				
 				{loading}
 				
-				<div className="showMessages">
+				<div onScroll={this.handleScroll.bind(this)} className="showMessages">
 					{this.showClients()}
 				</div>
 									
